0612e31be44dd0b108a81dc470f5596b6cacab2e
Maksym <max.yemelianenko@gmail.com>
2026-07-09 10:41:27 -0400

    update the deployment script to use systemd

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# kill all sessions without an error
-tmux kill-server || true
-
 cd /root/portfolio-maksym
 
 git fetch && git reset origin/main --hard
@@ -10,5 +7,6 @@ git fetch && git reset origin/main --hard
 source venv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s "portfolio" -c '/root/portfolio-maksym' 'source venv/bin/activate && flask run --host=0.0.0.0'
+systemctl daemon-reload
+systemctl restart myportfolio
 
